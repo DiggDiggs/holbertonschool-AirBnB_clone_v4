34422df6a11b34049f83fff33ad88f16cc58d0d8
DiggDiggs <christopherdiggs1@gmail.com>
2023-08-11 16:02:56 -0700

    sql/port work

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,30 +1,14 @@
-<head>
-    <!-- ... other head content ... -->
-    
-    <!-- Importing jQuery -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    
-    <!-- Importing the 1-hbnb.js script -->
-    <script src="static/scripts/1-hbnb.js" type="text/javascript"></script>
-</head>
+$(document).ready(function() {
+  let listOfCheckedAmenities = [];
 
-$(document).ready(function () {
-    // Dictionary to store the checked amenities
-    let checkedAmenities = {};
-  
-    // Listen for changes on input checkboxes
-    $('input[type="checkbox"]').change(function () {
-      // Check if checkbox is checked
-      if ($(this).is(':checked')) {
-        // Add amenity to the dictionary with id as the key
-        checkedAmenities[$(this).data('id')] = $(this).data('name');
-      } else {
-        // Remove amenity from the dictionary
-        delete checkedAmenities[$(this).data('id')];
-      }
-      // Update the h4 tag with the list of checked amenities
-      let amenityNames = Object.values(checkedAmenities);
-      $('div.amenities h4').text(amenityNames.join(', '));
-    });
+  $('li :checkbox').change( function () {
+    const dataName = $(this).attr("data-name");
+
+    if (this.checked) {
+      listOfCheckedAmenities.push(dataName);
+    } else {
+      listOfCheckedAmenities = listOfCheckedAmenities.filter((item) => item !== dataName);
+    }
+    $('div.amenities h4').text(listOfCheckedAmenities.join(', '));
   });
-  
+});
