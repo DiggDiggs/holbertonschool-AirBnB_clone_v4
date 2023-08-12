b1602d2392b9c956e861d13399f623fef395f940
christhecoolguy <christopherdiggs1@gmail.com>
2023-08-11 19:10:44 -0500

    trying to fix

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -2,17 +2,22 @@
 """ Starts a Flash Web Application """
 from models import storage
 from models.state import State
+from models.city import City
 from models.amenity import Amenity
 from models.place import Place
+from os import environ
 from flask import Flask, render_template
 import uuid
-
 app = Flask(__name__)
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
+
 
 @app.teardown_appcontext
 def close_db(error):
     """ Remove the current SQLAlchemy Session """
     storage.close()
+
 
 @app.route('/1-hbnb/', strict_slashes=False)
 def hbnb():
@@ -33,8 +38,8 @@ def hbnb():
     return render_template('1-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places,
-                           cache_id=str(uuid.uuid4()))  # Convert UUID to string
+                           places=places, cache_id=uuid.uuid4())
+
 
 if __name__ == "__main__":
     """ Main Function """
