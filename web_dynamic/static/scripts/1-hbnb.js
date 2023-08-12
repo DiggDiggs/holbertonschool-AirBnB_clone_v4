4210bac5c677920b0fd628c7de796d0cfd145d61
Night8Guy <robert@lionsroad.com>
2023-08-12 10:13:23 -0500

    web static

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -6,8 +6,6 @@ $(document).ready(function() {
   let amenities = {};
 
   $('.amenities input[type="checkbox"]').change(function() {
-    
-    
     if ($(this).is(':checked')) {
       amenities[$(this).data('id')] = $(this).data('name');
     } else {
@@ -17,7 +15,7 @@ $(document).ready(function() {
   });
 
   function updateAmenitiesList() {
-    let list = Object.values(amenities).join(', ');
-    $('.amenities h4').text(amenityList);
+    let list = Object.values(amenities).join(', '); // Changed variable name here
+    $('.amenities h4').text(list); // Use 'list' instead of 'amenityList'
   }
 });
