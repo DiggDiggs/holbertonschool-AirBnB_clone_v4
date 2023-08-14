f0b6da45e9221e294c65c5228880c86bfe0160ad
christhecoolguy <christopherdiggs1@gmail.com>
2023-08-14 10:08:04 -0500

    update

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,21 +1,30 @@
 #!/usr/bin/node
-// Only execute when DOM is loaded
-// Store Amenity, if checked
-// Remove Amenity if box is unchecked.
-$(document).ready(function() {
-  let amenities = {};
+// Execute only when DOM is loaded
+// Listen for changes on each input checkbox tag:
+// if the checkbox is checked, you must store the Amenity ID in a variable (dictionary or list)
+// if the checkbox is unchecked, you must remove the Amenity ID from the variable
+// update the h4 tag inside the div Amenities with the list of Amenities checked
 
-  $('.amenities input[type="checkbox"]').change(function() {
+window.onload = function() {
+  // Create empty object to store amenities
+  let checkedAmenities = {};
+
+  // Listen for changes on each input checkbox
+  $('input[type="checkbox"]').on('change', function() {
+    console.log("POP!")
+    console.log($(this).data('id'));
+    let myID = $(this).data('id');
+    let myName = $(this).data('name');
+    
     if ($(this).is(':checked')) {
-      amenities[$(this).data('id')] = $(this).data('name');
+   checkedAmenities[myID] = myName;
     } else {
-      delete amenities[$(this).data('id')];
+      if (checkedAmenities[myID]) {
+      delete checkedAmenities[myID];}
     }
-    updateAmenitiesList();
-  });
 
-  function updateAmenitiesList() {
-    let list = Object.values(amenities).join(', '); // Changed variable name here
-    $('.amenities h4').text(list); // Use 'list' instead of 'amenityList'
-  }
-});
+    let amenitiyList = Object.values(checkedAmenities).join(', ');
+    $('.amenities h4').text(amenitiyList);
+    
+  });
+};
